df690b8d1239a0e084c0703513ff39f53327bb6e
Nasir <nasir-islam@hotmail.com>
2021-10-12 21:29:21 +0100

    Added ability to change colour using colour picker

--- app.js
+++ app.js
@@ -1,48 +1,73 @@
-// Slider stuff
-let slider = document.getElementById("myRange");
-let output = document.getElementById("sliderOutput");
+//Variables
+let currentColour = "blue"
+
+//Get all HTML Elements
+let resolutionSlider = document.getElementById("myRange");
+let resolutionOutput = document.getElementById("sliderOutput");
 let gridContainer = document.getElementById("grid-container")
+let colourPicker = document.getElementById("colourPicker")
+
+
+colorPicker.addEventListener("change", watchColorPicker, false);
+
+function watchColorPicker(event) {
+  currentColour = event.target.value;
+  addColourToGrids();
+}
+
 
 // Display the default slider value
-output.innerHTML = `${slider.value} x ${slider.value}`;; 
 
-// Adding event listeners to each of the current divs to allow colouring on hover
-var grids = gridContainer.getElementsByTagName('*');
-var gridsList = Array.prototype.slice.call(grids);
-gridsList.forEach((grid) => {
-// and for each one we add a 'click' listener
-    grid.addEventListener('mouseover', () => {
-      grid.style.backgroundColor = 'green';
-    });
-  });
-
-
+updateResolutionOutput();
+reloadGrid();
+addColourToGrids();
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = `${this.value} x ${this.value}`;
-  //Make grid column and rows repeat using this.value!!
-  gridContainer.style.gridTemplateColumns = `repeat(${this.value},1fr)`;
-  gridContainer.style.gridTemplateRows = `repeat(${this.value},1fr)`;
+resolutionSlider.oninput = function () {
+  updateResolutionOutput();
+  reloadGrid();
+  addColourToGrids();
+}
 
-  //Now make as many divs and append it 
+
+function updateResolutionOutput() {
+  resolutionOutput.innerHTML = `${resolutionSlider.value} X ${resolutionSlider.value}`;
+}
+
+function reloadGrid() {
+  gridContainer.style.gridTemplateColumns = `repeat(${resolutionSlider.value},1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${resolutionSlider.value},1fr)`;
+  //Now make divs and append it 
   gridContainer.innerHTML = ''; // First clear all HTML to prevent acculmulation
-  for (let i = 0;i < this.value**2;++i)
-  {
+  for (let i = 0; i < resolutionSlider.value ** 2; ++i) {
     var div = document.createElement('div');
     gridContainer.appendChild(div);
   }
+}
 
-  // ADD FRESH events listener to each grid to allow colouring!!!
+// Adding event listeners to each of the current divs to allow colouring on hover
+function addColourToGrids() {
   var grids = gridContainer.getElementsByTagName('*');
   var gridsList = Array.prototype.slice.call(grids);
   gridsList.forEach((grid) => {
-      // and for each one we add a 'click' listener
-      grid.addEventListener('mouseover', () => {
-          grid.style.backgroundColor = 'green';
-        });
+    grid.addEventListener('mouseover', () => {
+      // grid.style.backgroundColor = getRandomColour();
+      console.log(colourPicker)
+      grid.style.backgroundColor = currentColour;
     });
-
+  });
 }
+
+
+
+function getRandomColour(){
+  const randomR = Math.floor(Math.random() * 256)
+  const randomG = Math.floor(Math.random() * 256)
+  const randomB = Math.floor(Math.random() * 256)
+  return `rgb(${randomR}, ${randomG}, ${randomB})`
+}
+
+
+
 
 
